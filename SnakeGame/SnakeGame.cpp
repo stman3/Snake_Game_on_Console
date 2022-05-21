ee985031226de237123ff3b1d01a423e6dcb79de
stman3 <75269916+stman3@users.noreply.github.com>
2022-05-22 02:17:53 +0300

    finish The movement of the player and some logic.

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -3,16 +3,24 @@
 using namespace std;
 
 bool GameOver;
+
+//map size
 const int width = 25;
 const int hight = 25;
-int x, y, score;
+
+//player position and score 
+int x, y,speed;
+
+//fruit position
 int fruitX, fruitY;
 
+
+//Game variables
 enum directions
 {
 	STOP =0,UP,DOWN,LEFT,RIGHT
 };
-
+int score;
 directions dir;
 
 void Setup() {
@@ -28,12 +36,14 @@ void Setup() {
 	}
 
 	score = 0;
+	speed = 1;
 }
 
 
 void Draw(){
 	system("cls");
 
+	//map,player and fruit
 	for (int i = 0; i < hight; i++) {
 		for (int j = 0; j < width; j++) {
 			if (i == 0 || i == width - 1) {
@@ -42,9 +52,16 @@ void Draw(){
 			else if (j == 0 || j == width - 1) {
 				cout << "*";
 			}
+			else if (i == y && j == x) {
+				cout << "0";
+			}
+			else if (i == fruitY && j == fruitX) {
+				cout << "f";
+			}
 			else {
 				cout << " ";
 			}
+			
 		}
 		cout << endl;
 	}
@@ -54,11 +71,60 @@ void Draw(){
 
 }
 void game_Input() {
+	if (_kbhit()) {
+		switch (_getch())
+		{
+		case 'a':
+			if (dir != RIGHT) {
+				dir = LEFT;
+			}
+			break;
+		case 'd':
+			if (dir != LEFT) {
+				dir = RIGHT;
+			}
+			break;
+		case 'w':
+			if (dir != DOWN) {
+				dir = UP;
+			}
+			break;
+		case 's':
+			if (dir != UP) {
+				dir = DOWN;
+			}
+			break;
+		case 'n':
+			GameOver = true;
+			break;
+		}
+
+	}
 
 }
 
 
 void logic() {
+	switch (dir)
+	{
+	case UP:
+		y -= speed;
+		break;
+	case DOWN:
+		y += speed;
+		break;
+	case LEFT:
+		x -= speed;
+		break;
+	case RIGHT:
+		x += speed;
+		break;
+	}
+
+
+	if (x <= 0 || x >= width - 1 || y <= 0 || y >= hight - 1) {
+		GameOver = true;
+	}
 
 }
 
@@ -66,10 +132,11 @@ void logic() {
 
 int main() {
 	Setup();
-	Draw();
-	game_Input();
-	logic();
-	while (!GameOver) {
+	while (!GameOver)
+	{
+		Draw();
+		game_Input();
+		logic();
 
 	}
 
