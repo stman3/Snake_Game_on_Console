8d0c5ea8fbc355b0828daf220bed9752fc7fed2c
stman3 <75269916+stman3@users.noreply.github.com>
2022-05-22 01:00:56 +0300

    Add project files.

---
+++ SnakeGame/SnakeGame.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+
+void Setup() {
+
+}
+
+
+void Drow(){
+
+}
+
+
+void game_Input() {
+
+}
+
+
+void logic() {
+
+}
+
+
+
+int main() {
+
+	return 0;
+}
